5dce8591ff95fb55ed8e252d8872f36450af236b
Tim Baggett <tim.baggett@gmail.com>
2025-10-04 22:28:23 -0500

    BUG: Fixing plca_status_timer duration. Was 130.090us, now 130.090ms

--- IEEE_P802d3da/D2p2/code/mod_148_4_6_timer.v
+++ IEEE_P802d3da/D2p2/code/mod_148_4_6_timer.v
@@ -21,11 +21,8 @@ output           plca_status_timer_not_done;
 /* plca_status_timer                                                    */
 /*                                                                      */
 
-parameter       plca_status_timer_duration_min = 130090;  // 130090 ns 
-parameter       plca_status_timer_duration_max = 140090;  // 140090 ns 
-
-// parameter       plca_status_timer_duration_min = 13009000; // 130 090 bit times
-// parameter       plca_status_timer_duration_max = 14009000; // 130 090 + 10 000 bit times
+parameter       plca_status_timer_duration_min = 13009000; // 130 090 bit times
+parameter       plca_status_timer_duration_max = 14009000; // 130 090 + 10 000 bit times
 
 IEEE802_3_timer #(plca_status_timer_duration_min, plca_status_timer_duration_max)
 plca_status_timer(
